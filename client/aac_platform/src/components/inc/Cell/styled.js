242c4dce9ecfda0f33a44c1d5e280885c814f03e
Victor Hugo Amaral <amaral.vht@gmail.com>
2024-09-25 13:18:44 -0300

    Ajuste de largura máxima da célula.

--- client/aac_platform/src/components/inc/Cell/styled.js
+++ client/aac_platform/src/components/inc/Cell/styled.js
@@ -5,6 +5,8 @@ export const CellContainer = styled.div`
   background-color: white;
   border-radius: 8px;
   filter: drop-shadow(0 4px 3px gray);
+  max-width: 265px;
+  max-height: 90px;
 
 
   &:hover {
