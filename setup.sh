85c69009639dccc0744df3aa1f2ae99f7003767d
José Guilherme <145349759+zeguilherme1@users.noreply.github.com>
2026-04-27 00:31:23 -0300

    Remove repo cloning from setup.sh

--- setup.sh
+++ setup.sh
@@ -7,10 +7,6 @@ sudo apt-get install -y git
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 
-# Cloning the repo
-git clone https://github.com/brunaru/vivavox
-cd ~/vivavox/
-
 # Installing npm packages	
 npm install
 npm run install:all
